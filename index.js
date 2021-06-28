5bd7eb2c149fae2aabb7da676ad1f9762cff5d68
Anil1824 <Anil1824@users.noreply.github.com>
2021-06-28 17:17:43 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+// var num=1;
+// while(num<=50){
+//     console.log(num);
+//     num++;
+// }
+ 
+// function evenNumbers(){
+//     for(var i=2;i<=100;i=i+2)
+//     console.log(i);
+// }
+// evenNumbers();
